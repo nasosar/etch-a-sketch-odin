4e885c82a6f685f4ffe821eed0a7e2d9c9ec5b16
Natasha <natashasosar@gmail.com>
2022-11-25 19:39:55 -0300

    Make functions on JS to display the grid acording to the selected scale

--- javascript.js
+++ javascript.js
@@ -1,10 +1,47 @@
+//interface functions: show the value on the slider range bar
+
 const slider = document.getElementById("rangebar");
 const valueSlider = document.getElementById("valuedisplay");
-
-console.log ("value is" + valueSlider);
+let gridValue = slider.value;
 
 valueSlider.innerHTML = (slider.value + " x " + slider.value);
+getGrid ();
 
 slider.oninput = function () {
     valueSlider.innerHTML = (this.value + " x " + this.value);
+    gridValue = slider.value;
+
+    getGrid ();
 }
+
+//grid functions
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function getGrid () {
+    
+    let columns = gridValue;
+    let rows = gridValue;
+    const gridCanva = document.querySelector('.div-canva');
+    let grid = document.createElement('div');
+
+    removeAllChildNodes(gridCanva);
+
+    grid.className = 'grid';
+    for (let i = 0; i < columns; ++i) {
+        let column = document.createElement('div'); // create column
+        column.className = 'column';
+        for (let j = 0; j < rows; ++j) {
+            let row = document.createElement('div'); // create row
+            row.className = 'row';
+            column.appendChild(row); // append row in column
+        }
+        grid.appendChild(column); // append column inside grid
+    }
+    gridCanva.appendChild(grid);    
+}
+
